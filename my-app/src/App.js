befd3afcbe8448f107dbf2198e280558b92c263c
Linnéa Ternevik Zackrisson <linnea.ternevik@gmail.com>
2024-07-13 15:45:14 +0000

    hooko challenge

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,8 @@ import css from './App.module.css';
 // import NavBarSimple from "./components/NavBarSimple";
 import Sidebar from "./components/Sidebar";
 import NavBarForm from './components/NavBarForm';
-import Content from './components/Content';
+// import Content from './components/Content';
+import ContentHooks from '.components/ContentHooks'
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       {/* <NavBarSimple /> */}
       <NavBarForm />
       <Sidebar />
-      <Content />
+      {/* <Content /> */}
+      <ContentHooks />
     </div>
   );
 }
