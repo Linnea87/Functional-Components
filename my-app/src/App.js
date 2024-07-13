1e4d1854a190db93e9cd3abda23e1f33dd61b42a
Linnéa Ternevik Zackrisson <linnea.ternevik@gmail.com>
2024-07-13 18:49:25 +0000

    Api challenge part 3

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,9 +2,11 @@ import css from './App.module.css';
 // import NavBarSimple from "./components/NavBarSimple";
 import Sidebar from "./components/Sidebar";
 import NavBarForm from './components/NavBarForm';
+import ContentAPIHooks from './components/ContentAPIHooks';
 // import Content from './components/Content';
 // import ContentHooks from './components/ContentHooks'
-import ContentAPI from './components/ContentAPI';
+// import ContentAPI from './components/ContentAPI';
+
 
 function App() {
   return (
@@ -15,7 +17,8 @@ function App() {
       <Sidebar />
       {/* <Content /> */}
       {/* <ContentHooks /> */}
-      <ContentAPI />
+      {/* <ContentAPI /> */}
+      <ContentAPIHooks />
     </div>
   );
 }
